710f80c4fa6954d1dc1469ee2db0d71bed78bf0c
E-estrella <5eunbyeol8@gmail.com>
2023-03-09 19:16:57 +0900

    파일 이름 수정

--- 230303.cpp
+++ 230303.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	int array[5][7] = { 1 }; /* 10행 10열의 배열 선언 */
+	int array[5][7] = { 1 }; /* 5행 7열의 배열 선언 */
 
 	printf("%d %d \n", &array[0][0], &array[3][4]); /* 해당 인덱스에 위치한 배열 주소 출력 */
 }
